30b112cb9aa9b5b9663e6de96bfd18d815ed447f
shivamchamoli09 <shivamchamoli09@gmail.com>
2024-01-24 14:48:39 +0530

    feat: update version with license upgrade

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
-import App from "./App";
-import "./index.css";
+// import App from "./App";
+// import "./index.css";
 
-import { createRoot } from "react-dom/client";
-import React from "react";
+// import { createRoot } from "react-dom/client";
+// import React from "react";
 
-const rootElement = document.getElementById("root") as any;
-const root = createRoot(rootElement);
-root.render(<App />);
-// export * from "./components";
+// const rootElement = document.getElementById("root") as any;
+// const root = createRoot(rootElement);
+// root.render(<App />);
+export * from "./components";
